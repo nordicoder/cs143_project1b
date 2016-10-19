695c25328cd224fd4a2d3c1d0c56484c8048d8d9
sachinkrishnabhat <sachinkrishnabhat@users.noreply.github.com>
2016-10-18 18:55:56 -0700

    Add files via upload

--- create.sql
+++ create.sql
@@ -1,37 +1,58 @@
+create table Movie(id int NOT NULL,title varchar(100) NOT NULL,year int,rating varchar(10),company varchar(50), 
+PRIMARY KEY(id))
+ ENGINE = INNODB;
 
 
+create table Actor(id int NOT NULL,last varchar(20),first varchar(20),sex varchar(6),dob date NOT NULL,dod date,
+PRIMARY KEY(id),
+CHECK(dob < dod)) ENGINE = INNODB;
 
 
-create table Movie(id int,title varchar(100),year int,rating varchar(10),company varchar(50) );
+create table Director(id int NOT NULL,last varchar(20),first varchar(20),dob date NOT NULL,dod date,
+PRIMARY KEY(id),
+CHECK(dob<dod)) ENGINE = INNODB;
 
 
+create table MovieGenre(mid int NOT NULL,genre varchar(20),
+PRIMARY KEY(mid, genre),
+FOREIGN KEY(mid) REFERENCES Movie(id)
+ON DELETE CASCADE 
+ON UPDATE CASCADE) ENGINE = INNODB;
 
 
-create table Actor(id int,last varchar(20),first varchar(20),sex varchar(6),dob date,dod date);
+create table MovieDirector(mid int,did int,
+PRIMARY KEY(mid, did),
+FOREIGN KEY(mid)  REFERENCES Movie(id)
+ON DELETE CASCADE 
+ON UPDATE CASCADE, 
+FOREIGN KEY(did)  REFERENCES Director(id)
+ON DELETE CASCADE 
+ON UPDATE CASCADE
+) ENGINE = INNODB;
 
 
-
-create table Director(id int,last varchar(20),first varchar(20),dob date,dod date);
-
-
-
-create table MovieGenre(mid int,genre varchar(20));
-
-
-
-
-create table MovieDirector(mid int,did int);
+create table MovieActor(mid int,aid int,role varchar(50),
+PRIMARY KEY(mid, aid, role),
+FOREIGN KEY(mid)  REFERENCES Movie(id)
+ON DELETE CASCADE 
+ON UPDATE CASCADE, 
+FOREIGN KEY(aid)  REFERENCES Actor(id) 
+ON DELETE CASCADE
+ON UPDATE CASCADE,
+CHECK ((select year from Movie where Movie.id = mid)<(select year(dob) from Actor where id = aid))  ) ENGINE = INNODB;
 
 
-create table MovieActor(mid int,aid int,role varchar(50));
+create table Review(name varchar(20),time timestamp,mid int,rating int,comment varchar(500),
+PRIMARY KEY(mid, name),
+FOREIGN KEY(mid) REFERENCES Movie(id)
+ON DELETE CASCADE
+ON UPDATE CASCADE,
+CHECK(rating between 0 and 5)) ENGINE = INNODB;
 
 
-create table Review(name varchar(20),time timestamp,mid int,rating int,comment varchar(500));
-
-create table MaxPersonID(id int);
+create table MaxPersonID(id int) ENGINE = INNODB;
 
 
-
-create table MaxMovieID(id int);
+create table MaxMovieID(id int) ENGINE = INNODB;
 
 
