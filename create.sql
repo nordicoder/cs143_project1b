10f97ca29cde88814f77fc20099ee4812395f5fe
nordicoder(Omkar Patil) <omkar.9194@gmail.com>
2016-10-18 19:43:43 -0700

    Added comments for the query.sql regarding constraints

--- create.sql
+++ create.sql
@@ -1,25 +1,29 @@
+#creates movie table with movie id being primary key, as there can be no two movies having same unique id
 create table Movie(id int NOT NULL,title varchar(100) NOT NULL,year int,rating varchar(10),company varchar(50), 
 PRIMARY KEY(id))
  ENGINE = INNODB;
 
-
+#creates actor table with actor id being primary key, as there can be no two actor having same unique id
 create table Actor(id int NOT NULL,last varchar(20),first varchar(20),sex varchar(6),dob date NOT NULL,dod date,
 PRIMARY KEY(id),
 CHECK(dob < dod)) ENGINE = INNODB;
 
-
+#creates director table with director id being primary key, as there can be no two directors having same unique id
 create table Director(id int NOT NULL,last varchar(20),first varchar(20),dob date NOT NULL,dod date,
 PRIMARY KEY(id),
 CHECK(dob<dod)) ENGINE = INNODB;
 
-
+#creates moviegenre table with movie id(mid) being foreign key as if a movie doesnt exist in the movie table, there no point writing
+#information about it in the moviegenre table
 create table MovieGenre(mid int NOT NULL,genre varchar(20),
 PRIMARY KEY(mid, genre),
 FOREIGN KEY(mid) REFERENCES Movie(id)
 ON DELETE CASCADE 
 ON UPDATE CASCADE) ENGINE = INNODB;
 
-
+#creates moviedirector table with movie id(mid) being foreign key as if a movie doesnt exist in the movie table, there no point writing
+#information about it in the moviedirector table
+#Also director id is a foreign key as if the director doesnt exist in the director table, he cant be a director of a movie
 create table MovieDirector(mid int,did int,
 PRIMARY KEY(mid, did),
 FOREIGN KEY(mid)  REFERENCES Movie(id)
@@ -30,7 +34,9 @@ ON DELETE CASCADE
 ON UPDATE CASCADE
 ) ENGINE = INNODB;
 
-
+#creates movieactor table with movie id(mid) being foreign key as if a movie doesnt exist in the movie table, there no point writing
+#information about it in the movieactor table
+#Also actor id is a foreign key as if the actor doesnt exist in the actor table, he cant be an actor in a movie
 create table MovieActor(mid int,aid int,role varchar(50),
 PRIMARY KEY(mid, aid, role),
 FOREIGN KEY(mid)  REFERENCES Movie(id)
@@ -41,7 +47,8 @@ ON DELETE CASCADE
 ON UPDATE CASCADE,
 CHECK ((select year from Movie where Movie.id = mid)<(select year(dob) from Actor where id = aid))  ) ENGINE = INNODB;
 
-
+#creates review table with movie id(mid) being foreign key as if a movie doesnt exist in the movie table, there no point writing
+#information about it in the review table
 create table Review(name varchar(20),time timestamp,mid int,rating int,comment varchar(500),
 PRIMARY KEY(mid, name),
 FOREIGN KEY(mid) REFERENCES Movie(id)
